4f24c069efe1306fab61fc69eb185fb1bbe8a837
Ajay <ajaymorde457@gmail.com>
2023-12-29 09:40:43 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './pages.css'
 
+
 const Home = () => {
   return (
     <div>
       <section className="home-section">
         <div className="home-wel">
-          <p>Welcome to <span className="home-wel-heading">Restoneed</span></p>
+          <div ><p>Welcome to <span className="home-wel-heading">Restoneed</span></p></div>
           <h2 className="head">Savor delicious meals <br/><span className='head1'>Trust us with technology</span></h2>
         </div>
 
