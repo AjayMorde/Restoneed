8262700d5fc4893680e0ecc2bd06a3dd907377f6
Ajay <ajaymorde457@gmail.com>
2023-12-31 19:11:05 +0530

    form updated

--- src/components/PageContent/HomeContent/HomeForm.js
+++ src/components/PageContent/HomeContent/HomeForm.js
@@ -1,10 +1,16 @@
+import ReCAPTCHA from "react-google-recaptcha";
 import React from 'react'
 import { useState } from 'react';
+import cont1 from '../../../images/ContactUs/cont2.jpg'
+
+const onChange=()=>{
+
+}
 
 const HomeForm = () => {
 
     const [selectedCountry, setSelectedCountry] = useState({
-        code: '+91', 
+        code: '+91',
         name: 'India'
     });
 
@@ -20,95 +26,131 @@ const HomeForm = () => {
 
     const countryOptions = [
         { code: '+91', name: 'India' },
-        
+
         { code: '+1', name: 'United States' },
         { code: '+44', name: 'United Kingdom' },
-        
+
     ];
     return (
 
         <>
+
+             <h1 style={{textAlign:'center',marginBottom:'80px', fontSize:'40px'}}>
+             Let's Begin with a Demo
+             </h1>
             <div className="home-section5">
-                <h2>Restaurant Details</h2>
-                <form >
-                    <input type="text"
-                        placeholder='Restaurent Name'
-                        id='restaurantName'
-                        required
-                    />
 
-                    <br /> <br />
-
-                    <input type="text"
-                        placeholder='Restaurent Manager Name'
-                        id=''
-                        required
-
-
-                    />
-
-                    <br />
-
-                    <select
-                        id="countryCode"
-                        name="countryCode"
-                        value={`${selectedCountry.code},${selectedCountry.name}`}
-                        onChange={handleCountryChange}
-                    >
-                        {countryOptions.map((country, index) => (
-                            <option key={index} value={`${country.code},${country.name}`}>
-                                {`${country.name} (${country.code})`}
-                            </option>
-                        ))}
-                    </select>
-
-                    <input
-                        type="tel"
-                        id="mobileNumber"
-                        name="mobileNumber"
-                        pattern="[+]91[0-9]{8}"
-                        placeholder="+91"
-                        required
-                    />
-
-                 
-
-                    <br />
-
-                    <input type="text"
-                        placeholder='Enter State'
-                        id=''
-                        required
-
-
-                    />
-                    <br />
-
-                    <input type="text"
-                        placeholder='Enter City'
-                        id=''
-                        required
-
-
-                    />
-                    <br />
-
-                        <input type="text"
-                        placeholder='Enter Address'
-                        id=''
-                        required
-
-
-                    />
-                    <br />
+                <div className='form1'>
 
                     
+                    <form >
+                        <input type="text"
+                            placeholder='Restaurent Name'
+                            id='restaurantName'
+                            required
+                        />
 
-                </form>
+                        <br /> <br />
+
+                        <input type="text"
+                            placeholder='Restaurent Manager Name'
+                            id=''
+                            required
+
+
+                        />
+
+                        <br /> <br />
+
+                        <select
+                            id="countryCode"
+                            name="countryCode"
+                            value={`${selectedCountry.code},${selectedCountry.name}`}
+
+                            onChange={handleCountryChange}
+                        >
+                            {countryOptions.map((country, index) => (
+                                <option key={index} value={`${country.code},${country.name}`}>
+                                    {`${country.name} (${country.code})`}
+                                </option>
+                            ))}
+                        </select>
+                        <br /> <br />
+
+                        <input
+                            type="tel"
+                            id="mobileNumber"
+                            name="mobileNumber"
+                            pattern="[+]91[0-9]{8}"
+                            placeholder="Manager Mobile Number"
+                            required
+                        />
+
+
+
+                        <br /> <br />
+
+                        <input type="text"
+                            placeholder='Enter State'
+                            id=''
+                            required
+
+
+                        />
+                        <br /> <br />
+
+                        <input type="text"
+                            placeholder='Enter City'
+                            id=''
+                            required
+
+
+                        />
+                        <br /> <br />
+
+                        <input type="text"
+                            placeholder='Enter Address'
+                            id=''
+                            required
+
+
+                        />
+                        <br /> <br />
+
+                        <ReCAPTCHA
+                        className="capcha"
+                           sitekey="6LfXmEEpAAAAAI5trHR57Fde-9BabKsmGdIAr69f"
+                           onChange={onChange}
+                        />,   
+
+                        <br /> <br />
+
+                        <button>Submit</button>
+
+
+
+
+                    </form>
+
+                </div>
+
+
+                <div className='img'>
+                    <img src={cont1} alt="demo-img" />
+                </div>
+
+
+
+
 
             </div>
 
+
+
+
         </>
+
+
 
     )
 }
